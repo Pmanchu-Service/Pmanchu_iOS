8f7b932beb13f229dcaeaae4490cb7c8e63fe870
heehee <leejh080628@naver.com>
2024-09-18 22:59:09 +0900

    J :: 프로젝트 세팅

--- Pmanchu/Pmanchu/Sources/Login/LoginViewController.swift
+++ Pmanchu/Pmanchu/Sources/Login/LoginViewController.swift
@@ -54,7 +54,6 @@ class LoginViewController: UIViewController {
         $0.semanticContentAttribute = .forceLeftToRight
         $0.contentHorizontalAlignment = .center
             }
-
     
     
     
