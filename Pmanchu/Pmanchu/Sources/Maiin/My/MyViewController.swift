0aa92dae930d54213748dea040cfd979c19e6ba2
heehee <leejh080628@naver.com>
2024-10-15 17:32:39 +0900

    chore :: 파일 수정

---
+++ Pmanchu/Pmanchu/Sources/Maiin/My/MyViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+import SnapKit
+import Then
+
+class MyViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+}
